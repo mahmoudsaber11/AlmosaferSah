114dee399f0a80983b8bbdcd30f4eff7c0994661
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-02 21:05:08 +0200

    add assets on boarding and logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,11 @@
-void main(List<String> args) {}
+import 'package:almosafer_sah/almosafer_app.dart';
+import 'package:almosafer_sah/service_locator.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await ServiceLocator().setupServiceLocator();
+
+  runApp(const AlmosaferApp());
+}
