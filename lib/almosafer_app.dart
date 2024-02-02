68a204cc99d4f717ed48a07c28befa16604d2e55
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-02 22:10:02 +0200

    create file 'app assets'

--- lib/almosafer_app.dart
+++ lib/almosafer_app.dart
@@ -1,4 +1,6 @@
+import 'package:almosafer_sah/generated/l10n.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AlmosaferApp extends StatelessWidget {
@@ -6,11 +8,18 @@ class AlmosaferApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ScreenUtilInit(
-      designSize: Size(375, 812),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
       minTextAdapt: true,
       splitScreenMode: true,
       child: MaterialApp(
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
         debugShowCheckedModeBanner: false,
       ),
     );
---
+++ lib/core/utils/functions/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class AppAssets {
+  static const String appLogoEnglish = "assets/images/logoEnglish.png";
+  static const String appLogoArabic = "assets/images/logoArabic.png";
+
+  static const String onBoarding1 = "assets/images/onBoarding1.png";
+  static const String onBoarding2 = "assets/images/onBoarding2.png";
+  static const String onBoarding3 = "assets/images/onBoarding3.png";
+}
