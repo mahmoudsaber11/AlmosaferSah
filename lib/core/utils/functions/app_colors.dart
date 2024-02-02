70bd118c17de8562f5de6960ec136a0beeabf9f5
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-02 22:17:48 +0200

    add file "app colors"

---
+++ lib/core/utils/functions/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xFFF07E01);
+  static const Color scaffoldBackgroundColor = Colors.white;
+  static const Color textColor = Color(0xFF2B3455);
+  static const Color greyColor = Color(0xFFDADADA);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+}
