9bec6282ca86e669abd225e789882bb8096318dd
(mahmoud <(mahmoudsaber223015@gmail.com)>
2024-02-02 22:19:46 +0200

    create file 'app string'

---
+++ lib/core/utils/functions/app_string.dart
@@ -0,0 +1,8 @@
+class AppString {
+  static const String appTitle = 'almosaferSah';
+  static const String cachedUserId = 'uId';
+  static const String cachedOnboarding = 'onBoarding';
+  static const String noInternet = 'No Internet Connection';
+  static const String opps =
+      "Opps something went wrong, please try again later!";
+}
